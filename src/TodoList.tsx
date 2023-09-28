d409ef0b0a01c9df431aa3fa7bd086df4f8bd66f
Yo sup <dhdytjq2@gamil.com>
2023-09-29 02:02:26 +0900

    TO_DO category - useRecoilState

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,92 +1,53 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 import styled from "styled-components";
 
+interface IToDo {
+  categories: "TO_DO" | "DOING" | "DONE";
+  id: number;
+  text: string;
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  password: string;
-  passwordConfirm: string;
+  toDo: string;
 }
 
 function TodoList() {
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
+  const [toDos, setToDos] = useRecoilState(toDoState);
 
-  console.log(errors);
-  const onValid = (data: IForm) => {
-    if (data.password !== data.passwordConfirm) {
-      setError(
-        "passwordConfirm",
-        { message: "please confirm your password" },
-        { shouldFocus: true }
-      );
-    }
-    console.log(data);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+
+  const onValid = ({ toDo }: IForm) => {
+    console.log(toDos);
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), categories: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
   };
   return (
     <div>
-      <FormWrapper onSubmit={handleSubmit(onValid)}>
+      <h1>TO_DO</h1>
+      <form onSubmit={handleSubmit(onValid)}>
         <input
-          {...register("email", {
-            required: "You should fill this FormWrapper",
-            pattern: {
-              value: /[A-Za-z0-9._%+-]+@naver.com/,
-              message: "Only naver.com is allowed",
-            },
+          {...register("toDo", {
+            required: "You should fill this form",
           })}
           placeholder="enter your email"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "You should fill this form",
-            validate: (value) =>
-              value.includes("yosup")
-                ? "you cannot use that name : yosup"
-                : false,
-          })}
-          placeholder="enter your first name"
-        />
-        <span>{errors?.firstName?.message}</span>
-
-        <input
-          {...register("lastName", { required: "You should fill this form" })}
-          placeholder="enter your last name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("password", {
-            required: "You should fill this form",
-            minLength: { value: 5, message: "your password is too short" },
-          })}
-          placeholder="enter your password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("passwordConfirm", {
-            required: "You should fill this form",
-            minLength: { value: 5, message: "your password is too short" },
-          })}
-          placeholder="enter your email"
-        />
-        <span>{errors?.passwordConfirm?.message}</span>
         <button>Enter</button>
-      </FormWrapper>
+      </form>
+      <ul>
+        {toDos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-const FormWrapper = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
 
 export default TodoList;
