14e51690c659e41dd36a027ff18c585ad9f4f6b3
Yo sup <dhdytjq2@gamil.com>
2023-10-11 01:41:13 +0900

    create

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
@@ -39,6 +39,22 @@ const Area = styled.div<IAreaProps>`
 `;
 const Form = styled.form`
   width: 100%;
+  margin-top: 25px;
+`;
+const BtnWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 25px;
+`;
+const Btn = styled.div`
+  width: 30px;
+  cursor: pointer;
+`;
+
+const CreateInput = styled.input`
+  width: 100%;
+  padding: 10px;
 `;
 
 interface IForm {
@@ -52,30 +68,33 @@ interface IBoardProps {
 function Board({ todos, droppaleId }: IBoardProps) {
   const { register, setValue, handleSubmit } = useForm<IForm>();
   const setTodos = useSetRecoilState(toDoState);
+  const [createTodo, setCreateTodo] = useState(false);
+
+  const toggleCreateTodo = () => {
+    setCreateTodo((prev) => !prev);
+  };
+
   const onValid = ({ todo }: IForm) => {
     const newTodo = {
       todoId: Date.now(),
       todoText: todo,
     };
+    console.log(todo);
     setTodos((allBoards) => {
       return {
         ...allBoards,
-        [droppaleId]: [newTodo, ...allBoards[droppaleId]],
+        [droppaleId]: [...allBoards[droppaleId], newTodo],
       };
     });
+
     setValue("todo", "");
+    setCreateTodo(false);
   };
 
   return (
     <Wrapper>
       <Title>{droppaleId}</Title>
-      <Form onSubmit={handleSubmit(onValid)}>
-        <input
-          {...register("todo", { required: true })}
-          type="text"
-          placeholder="할 일을 입력하세요"
-        />
-      </Form>
+
       <Droppable droppableId={droppaleId}>
         {(provided, snapshot) => (
           <Area
@@ -93,6 +112,53 @@ function Board({ todos, droppaleId }: IBoardProps) {
                 index={index}
               />
             ))}
+            {createTodo ? (
+              <Form onSubmit={handleSubmit(onValid)}>
+                <CreateInput
+                  {...register("todo", { required: true })}
+                  type="text"
+                  placeholder="할 일을 입력하세요"
+                />
+                <BtnWrapper>
+                  <Btn onClick={toggleCreateTodo}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="0.3"
+                      stroke="currentColor"
+                      className="w-1 h-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </Btn>
+                </BtnWrapper>
+              </Form>
+            ) : (
+              <BtnWrapper>
+                <Btn onClick={toggleCreateTodo}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="0.3"
+                    stroke="currentColor"
+                    className="w-1 h-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </Btn>
+              </BtnWrapper>
+            )}
+
             {provided.placeholder}
           </Area>
         )}
